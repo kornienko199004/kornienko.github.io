71d0b82be361c54b0ce6c3784a7354341ff78b9a
aleksey.kornienko <aleksey.kornienko@simbirsoft.com>
2022-07-18 21:14:13 +0300

    add router-outlet

--- src/app/payment/payment-routing.module.ts
+++ src/app/payment/payment-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreditCardModule } from './credit-card/credit-card.module';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: PaymentComponent,
     children: [
       {
         path: 'credit-card',
         loadChildren: () => import('./credit-card/credit-card.module').then((module) => module.CreditCardModule),
-      },
-      {
-        path: '',
-        redirectTo: '/payment/credit-card',
-        pathMatch: 'full',
       },
     ]
   }
--- src/app/payment/payment.module.ts
+++ src/app/payment/payment.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaymentRoutingModule } from './payment-routing.module';
+import { PaymentComponent } from './payment/payment.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaymentComponent
+  ],
   imports: [
     CommonModule,
-    PaymentRoutingModule
+    PaymentRoutingModule,
+    RouterModule
   ]
 })
 export class PaymentModule { }
